27db9efafff6a7911cff7e468976133fd83c4554
KeeleHauledDBA <keelehauleddba@gmail.com>
2018-01-14 19:38:17 -0700

    FInal submission

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # getting-cleaning-data-project
-Getting and Cleaning Data Course Final Project
+## Getting and Cleaning Data Project
+
+### Overview
+This project serves to demonstrate the collection and cleaning of a tidy data set that can be used for subsequent
+analysis. A full description of the data used in this project can be found at [The UCI Machine Learning Repository](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones)
+
+[The source data for this project can be found here.](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip)
+
+### Making Modifications to This Script
+All package called in this repository are available from cran. If you find you do not have the appropriate libraries as referanced please use install.packages("<name of the package") prior to running the code.
+
+### Assignment Goals
+The following is a summary description of the project instructions
+
+Create one R script called run_analysis.R that does the following. 
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive activity names. 
+5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+
+### Data Specific Information
+You can find additional information about the variables, data and transformations in the CodeBook.MD file.
--- run_analysis.R
+++ run_analysis.R
@@ -15,10 +15,11 @@
 #Appropriately labels the data set with descriptive variable names.
 #From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
+##Libraries
+library(reshape2)
+library(dplyr)
 
 ## 1. Get the data
-library(reshape2)
-
 filename <- "data/FUCIHAR_dataset.zip"
 if (!file.exists(filename)){
   fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
@@ -27,4 +28,74 @@ if (!file.exists(filename)){
 ##Unzip said data
 unzip(filename) 
 
+##Merges the training and the test sets to create one data set.
+
+##Getting our pertinent data sorted out: 
+# Trainings tables:
+x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
+
+# Testing tables:
+x_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
+
+# Features:
+features <- read.table('./data/UCI HAR Dataset/features.txt')
+
+# Activity labels:
+activitylabels = read.table('./data/UCI HAR Dataset/activity_labels.txt')
+
+##Getting column names assigned properly
+colnames(x_train) <- features[,2] 
+colnames(y_train) <-"activityId"
+colnames(subject_train) <- "subjectId"
+
+colnames(x_test) <- features[,2] 
+colnames(y_test) <- "activityId"
+colnames(subject_test) <- "subjectId"
+
+colnames(activitylabels) <- c('activityId','activityType')
+
+#####Merges the training and the test sets to create one data set.
+
+##Now that we know what to call everything we can bring it all together
+cb_train <- cbind(y_train, subject_train, x_train)
+cb_test <- cbind(y_test, subject_test, x_test)
+fullraw <- rbind(cb_train, cb_test)
+
+collabels <- colnames(fullraw)
+
+###Extracts only the measurements on the mean and standard deviation for each measurement.
+
+##Now for the smaller subset of measurements on the mean and standard deviation for each measurement
+colkeeping <- grepl("subject|activity|mean|std", colnames(fullraw))
+tidyish <- fullraw[, colkeeping]
+
+#Clean up a bit now that we have what we need to work with
+rm(fullraw, x_train, y_train, subject_train, x_test, y_test, subject_test)
+
+###Appropriately labels the data set with descriptive variable names.
+
+#Fix our labels so it makes sense a bit to the rest of the world
+tidycolnames <- colnames(tidyconlabs)
+
+##Special character handling and reassign fixed headers
+tidycolnames <- gsub("[\\(\\)-]", "", tidycolnames)
+colnames(tidyconlabs) <- tidycolnames
+
+###From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+tidydatawin <- aggregate(. ~subjectId + activityId, tidyconlabs, mean)
+tidydatawin <- tidydatawin[order(tidydatawin$subjectId, tidydatawin$activityId),]
+
+
+###Uses descriptive activity names to name the activities in the data set
+
+#Handle descriptive activity names
+tidied <- merge(tidydatawin, activitylabels, by='activityId', all.x=TRUE)
+
+###From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+write.table(tidied, "tidied_data.txt", row.name=FALSE)
 
